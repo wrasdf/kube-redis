4db3b266531c4d7a88837d336bc71c88793b7e6c
kerry.wang <wrasdf@gmail.com>
2018-04-09 00:34:49 +1000

    Test no configMap case for on_delete & on_modified controller

--- operator/tests/e2e/worker/test_on_deleted_controller.py
+++ operator/tests/e2e/worker/test_on_deleted_controller.py
@@ -16,6 +16,12 @@ class TestOnDeletedController(unittest.TestCase):
         if self.cfg_manager.exist_namespaced_config_map(self.cfg_name):
             self.cfg_manager.delete_namespaced_config_map(self.cfg_name)
 
+    def test_on_deleted_controller_should_throw_error(self):
+        with self.assertRaisesRegexp(ValueError, self.cfg_name + ' Should exist'):
+            OnDeletedController({
+                "name": self.co_name,
+                "namespace": self.namespace
+            })
 
     def test_on_deleted_controller_should_delete_cfg(self):
         self.cfg_manager.create_namespaced_config_map(self.cfg_name, {"created": "already"})
